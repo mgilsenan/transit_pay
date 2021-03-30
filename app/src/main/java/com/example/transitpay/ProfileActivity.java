e592cc523973112cafb8fcea7233331148b4b2d3
Jin <jino1101@hotmail.com>
2021-03-30 18:05:13 -0400

    Added tentative Profile page: display user's info on screen. Needs to update  firebase and update local user obj when saveBtn clicked

--- app/src/main/java/com/example/transitpay/ProfileActivity.java
+++ app/src/main/java/com/example/transitpay/ProfileActivity.java
@@ -4,24 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ProfileActivity extends AppCompatActivity {
 
     TextView profile;
     Button mainMenuButton;
+    TextInputEditText name;
+    TextInputEditText phone;
+    TextInputEditText email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        mainMenuButton=findViewById(R.id.mainMenuButton);
+        // hook
         profile = findViewById(R.id.userProfileTxt);
+        mainMenuButton=findViewById(R.id.saveBn);
+        name = findViewById(R.id.nameTxt);
+        phone = findViewById(R.id.phoneTxt);
+        email = findViewById(R.id.emailTxt);
 
         displayProfile();
+
+        // disable editing  
+        name.setInputType(InputType.TYPE_NULL);
 
         mainMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +48,10 @@ public class ProfileActivity extends AppCompatActivity {
 
     protected void displayProfile(){
 
-        profile.append("phone: " + LoginActivity.getUser().toString() + "\n");
+        name.setText(LoginActivity.getUser().getName());
+        phone.setText(LoginActivity.getUser().getPhone());
+        email.setText(LoginActivity.getUser().getEmail());
+
     }
 
     private void goToMainMenuActivity(){
