d602db3545cde1a97b525cbb8b048d7b56057883
Linfeng266 <71446857+Linfeng266@users.noreply.github.com>
2021-04-10 20:25:13 -0400

    Update MainMenuActivity.java

--- app/src/main/java/com/example/transitpay/MainMenuActivity.java
+++ app/src/main/java/com/example/transitpay/MainMenuActivity.java
@@ -2,32 +2,33 @@ package com.example.transitpay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import static java.lang.Integer.parseInt;
+
 public class MainMenuActivity extends AppCompatActivity {
-
-    private static String TAG = "MainMenuActivity";
-
     protected Button triphistoryButton;
     protected Button locationButton;
     protected Button purchaseButton;
     protected Button citylinesButton;
-//    protected Button activateButton;
+    DatabaseReference childNode;
+    private String remainingday="0";
+    private String remainingticket="0";
+    private TextView fareviewtext;
+    private ImageButton fareviewchangebtn;
+
+
 
 
     @Override
@@ -36,12 +37,17 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         setupUI();
     }
+
+
     private void setupUI(){
         triphistoryButton=findViewById(R.id.triphistoryButton);
         locationButton=findViewById(R.id.LocationButton);
         citylinesButton=findViewById(R.id.citylinesButton);
         purchaseButton=findViewById(R.id.purchasefareButton);
-//        activateButton = findViewById(R.id.activationcardButton);
+        fareviewtext = findViewById(R.id.fareviewtxt);
+        fareviewchangebtn = findViewById(R.id.fareviewchangebtn);
+
+
 
         triphistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,15 +76,93 @@ public class MainMenuActivity extends AppCompatActivity {
                 goToCityLinesActivity();
             }
         });
-//        activateButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                goToActivateCardActivity();
-//            }
-//        });
+
+        setRemainingfare();
+
+        fareviewchangebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if((fareviewtext.getText()).equals(remainingday + " Days Left")||(fareviewtext.getText()).equals(remainingday + " Day Left")){
+                    if(parseInt(remainingticket)>1){
+                        textflip();
+                        fareviewtext.setText(remainingticket + " Trips Left");
+                    }
+                    else {
+                        textflip();
+                        fareviewtext.setText(remainingticket + " Trip Left");
+                    }
+                }
+                else if((fareviewtext.getText()).equals(remainingticket + " Trips Left")||(fareviewtext.getText()).equals(remainingticket + " Trip Left")){
+                    if(parseInt(remainingday)>1){
+                        textflip();
+                        fareviewtext.setText(remainingday + " Days Left");
+                    }
+                    else {
+                        textflip();
+                        fareviewtext.setText(remainingday + " Day Left");
+                    }
+                }
+            }
+        });
+
+
+
+
 
 
     }
+    public void textflip(){
+        fareviewtext.animate().rotationXBy(360f);
+
+    }
+
+    private void setRemainingfare() {
+        childNode=FirebaseDatabase.getInstance().getReference().child("user").child(LoginActivity.getUser().getPhone());
+        childNode.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if(snapshot.child("ticketsLeft").exists()){
+                    remainingticket = snapshot.child("ticketsLeft").getValue().toString();
+                    if(parseInt(remainingticket)>1){
+                        fareviewtext.setText(remainingticket + " Trips Left");
+                    }
+                    else {
+                        fareviewtext.setText(remainingticket + " Trip Left");
+                    }
+
+                }
+                else {
+                    fareviewtext.setText(remainingticket + " Trip Left");
+                }
+
+                if (snapshot.child("daysLeft").exists()) {
+                    remainingday = snapshot.child("daysLeft").getValue().toString();
+                    if(parseInt(remainingday)>1){
+                        fareviewtext.setText(remainingday + " Days Left");
+                    }
+                    else {
+                        fareviewtext.setText(remainingday + " Day Left");
+                    }
+                }
+                else {
+                    fareviewtext.setText(remainingday + " Day Left");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
+
+
+
     private void goToTripHistoryActivity(){
         String phone_number=getIntent().getStringExtra("Phone number");
         Intent intent= new Intent(MainMenuActivity.this, TripHistoryActivity.class);
@@ -99,77 +183,6 @@ public class MainMenuActivity extends AppCompatActivity {
         Intent intent= new Intent(MainMenuActivity.this, CitylinesActivity.class);
         startActivity(intent);
     }
-    private void  goToActivateCardActivity(){
-        Intent intent= new Intent(MainMenuActivity.this, ActivateCardActivity.class);
-        startActivity(intent);
-    }
-
-
-    // Logout option------------------------------------------------------------------------------------------------
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // set the menu icon of the page with layout xml
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.profile_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        final Intent[] intent = {null}; // value of the intent is depends on the user selected button option
-        switch(item.getItemId()){
-            case R.id.logout:
-                Toast.makeText(this, "Logged out from account", Toast.LENGTH_LONG).show();
-                final String phonStr = LoginActivity.getUser().getPhone();
-                FirebaseDatabase.getInstance().getReference("user").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-
-                        if (snapshot.child(phonStr).exists() ) {
-
-                            // reset the flag
-                            FirebaseDatabase.getInstance().getReference("user").child(phonStr).child("loginBefore").setValue("FALSE");
-
-                            // destroy local user obj and return to login activity
-                            LoginActivity.getUser().setPhone("");
-                            LoginActivity.getUser().setName("");
-                            LoginActivity.getUser().setEmail("");
-                            LoginActivity.clearUser(); // removed the user in the preference file
-
-                            SharedPreferences pref = getSharedPreferences(LoginActivity.myPreference, Context.MODE_PRIVATE);
-                            String empty = pref.getString(LoginActivity.userPhone, "");
-                            String empty2 = pref.getString(LoginActivity.userPassword, "");
-                            Log.d(TAG,"the value of shared preference after LogOut is: " + empty );
-                            Log.d(TAG,"the value of shared preference after LogOut is: " + empty2 );
-                            intent[0] = new Intent(MainMenuActivity.this, LoginActivity.class);
-                            startActivity(intent[0]);
-                            finish();
-                        }else{
-                            Toast.makeText(MainMenuActivity.
-                                            this, "Phone number is not linked to an account",
-                                    Toast.LENGTH_LONG).show();
-                        }
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-                return true;
-            case R.id.profile:
-                intent[0]= new Intent(MainMenuActivity.this, ProfileActivity.class);
-                startActivity(intent[0]);
-                return true;
-            case R.id.cardActivation:
-                intent[0]= new Intent(MainMenuActivity.this, ActivateCardActivity.class);
-                startActivity(intent[0]);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
 
 
 }
