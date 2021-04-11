38dd718c3d00af40a829d4286110ead79965e5ca
Linfeng266 <71446857+Linfeng266@users.noreply.github.com>
2021-04-10 20:50:22 -0400

    Update CitylinesActivity.java

--- app/src/main/java/com/example/transitpay/CitylinesActivity.java
+++ app/src/main/java/com/example/transitpay/CitylinesActivity.java
@@ -1,6 +1,8 @@
 package com.example.transitpay;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.github.chrisbanes.photoview.PhotoView;
 
 public class CitylinesActivity extends AppCompatActivity {
+
+    private Button backtomainmenubtn;
+
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,13 @@ public class CitylinesActivity extends AppCompatActivity {
         PhotoView photoView = (PhotoView)
                 findViewById(R.id.photo_view);
         photoView.setImageResource(R.drawable.mtlmetro);
+        backtomainmenubtn=findViewById(R.id.backtomainbtn);
+
+        backtomainmenubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
