b8890369e40d64a21a8b8dccdd6aab3f23bd305d
Linfeng266 <71446857+Linfeng266@users.noreply.github.com>
2021-04-10 20:26:55 -0400

    Update MainMenuActivity.java

--- app/src/main/java/com/example/transitpay/MainMenuActivity.java
+++ app/src/main/java/com/example/transitpay/MainMenuActivity.java
@@ -2,12 +2,19 @@ package com.example.transitpay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,6 +25,9 @@ import com.google.firebase.database.ValueEventListener;
 import static java.lang.Integer.parseInt;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private static String TAG = "MainMenuActivity";
+
     protected Button triphistoryButton;
     protected Button locationButton;
     protected Button purchaseButton;
@@ -30,15 +40,12 @@ public class MainMenuActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         setupUI();
     }
-
-
     private void setupUI(){
         triphistoryButton=findViewById(R.id.triphistoryButton);
         locationButton=findViewById(R.id.LocationButton);
@@ -105,11 +112,6 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
     }
     public void textflip(){
         fareviewtext.animate().rotationXBy(360f);
@@ -157,10 +159,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-
-
     }
-
 
 
     private void goToTripHistoryActivity(){
@@ -183,6 +182,77 @@ public class MainMenuActivity extends AppCompatActivity {
         Intent intent= new Intent(MainMenuActivity.this, CitylinesActivity.class);
         startActivity(intent);
     }
+    private void  goToActivateCardActivity(){
+        Intent intent= new Intent(MainMenuActivity.this, ActivateCardActivity.class);
+        startActivity(intent);
+    }
+
+
+    // Logout option------------------------------------------------------------------------------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // set the menu icon of the page with layout xml
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        final Intent[] intent = {null}; // value of the intent is depends on the user selected button option
+        switch(item.getItemId()){
+            case R.id.logout:
+                Toast.makeText(this, "Logged out from account", Toast.LENGTH_LONG).show();
+                final String phonStr = LoginActivity.getUser().getPhone();
+                FirebaseDatabase.getInstance().getReference("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+
+                        if (snapshot.child(phonStr).exists() ) {
+
+                            // reset the flag
+                            FirebaseDatabase.getInstance().getReference("user").child(phonStr).child("loginBefore").setValue("FALSE");
+
+                            // destroy local user obj and return to login activity
+                            LoginActivity.getUser().setPhone("");
+                            LoginActivity.getUser().setName("");
+                            LoginActivity.getUser().setEmail("");
+                            LoginActivity.clearUser(); // removed the user in the preference file
+
+                            SharedPreferences pref = getSharedPreferences(LoginActivity.myPreference, Context.MODE_PRIVATE);
+                            String empty = pref.getString(LoginActivity.userPhone, "");
+                            String empty2 = pref.getString(LoginActivity.userPassword, "");
+                            Log.d(TAG,"the value of shared preference after LogOut is: " + empty );
+                            Log.d(TAG,"the value of shared preference after LogOut is: " + empty2 );
+                            intent[0] = new Intent(MainMenuActivity.this, LoginActivity.class);
+                            startActivity(intent[0]);
+                            finish();
+                        }else{
+                            Toast.makeText(MainMenuActivity.
+                                            this, "Phone number is not linked to an account",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                return true;
+            case R.id.profile:
+                intent[0]= new Intent(MainMenuActivity.this, ProfileActivity.class);
+                startActivity(intent[0]);
+                return true;
+            case R.id.cardActivation:
+                intent[0]= new Intent(MainMenuActivity.this, ActivateCardActivity.class);
+                startActivity(intent[0]);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
 
 }
