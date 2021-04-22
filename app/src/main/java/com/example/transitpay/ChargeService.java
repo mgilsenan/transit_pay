cda02d17df41555b2572d816cc196a54e2103e1f
Maryam Gilsenan <maryamgilsenan@gmail.com>
2021-04-22 18:43:43 -0400

    small edits

--- app/src/main/java/com/example/transitpay/ChargeService.java
+++ app/src/main/java/com/example/transitpay/ChargeService.java
@@ -5,7 +5,7 @@ import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface ChargeService {
-  @POST("/chargeForCookie")
+  @POST("/chargeTransitUser")
   Call<Void> charge(@Body ChargeRequest request);
 
   class ChargeErrorResponse {
